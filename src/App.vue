9aa4e68a3c6235a7a9e9997b148624517203b099
Engin Canik <engincanik@gmail.com>
2021-10-22 14:20:48 +0300

    Initial Commit
    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar variant="faded" type="light" style="background-color: #F5F5F5">
+
+      <b-navbar-brand href="#">
+        <img src="@/assets/logo.png" width="48px" height="48px" class="d-inline-block align-top" alt="Kitten">
+      </b-navbar-brand>
+
+      <b-collapse is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <i class="fa fa-shopping-basket" aria-hidden="true"/>
+              <b-badge class="mx-3" size="lg" variant="light">{{ totalItems }}</b-badge>
+            </template>
+            <b-dropdown-form style="width: 450px" v-for="(item, index) in selectedItems" :key="'selected item ' + index"
+                             class="d-flex align-items-center">
+              <b-row class="m-0 justify-content-center">
+                <b-col cols="3" class="align-items-center">
+                  {{ item.title }}
+                </b-col>
+                <b-col cols="1" class="text-center">
+                  {{ item.size }}
+                </b-col>
+                <b-col class="text-center" cols="4">
+                  {{ item.price * item.shoeCount }} $
+                </b-col>
+                <b-col cols="3" class="d-flex align-items-center">
+                  <span @click="item.shoeCount > 1 ? item.shoeCount-- : deleteItem(index)" class="fa fa-minus mx-3 "
+                        aria-hidden="true"></span>
+                  <span>{{ item.shoeCount }}</span>
+                  <span @click="item.shoeCount++" class="fa fa-plus mx-3 " aria-hidden="true"></span>
+                </b-col>
+                <b-col cols="1">
+                  <span role="button" class="fa fa-times mx-3" @click="deleteItem(index)"></span>
+                </b-col>
+              </b-row>
+            </b-dropdown-form>
+            <b-col class="text-center">
+              Total: {{calculateTotalPrice}}$
+            </b-col>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+
+    </b-navbar>
+    <b-row class="justify-content-around p-3">
+      <ShoeCard v-for="(product, i) in productList" :key="'product index'+i" :item="product" @basket="addToBasket"/>
+    </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoeCard from "@/components/ShoeCard";
+import {products} from "@/assets/products";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShoeCard
+  },
+  data() {
+    return {
+      productList: products,
+      selectedItems: []
+    }
+  },
+  computed: {
+    calculateTotalPrice() {
+      let totalPrice = 0
+      if(this.selectedItems.length > 0) {
+        this.selectedItems.forEach(item => {
+          totalPrice += item.price
+        })
+      }
+      return totalPrice
+    },
+    totalItems() {
+      let totalItemCount = 0;
+      this.selectedItems.forEach(item => {
+        totalItemCount += item.shoeCount
+      })
+      return totalItemCount
+    }
+  },
+  methods: {
+    addToBasket(e) {
+      let isAlreadySelected = this.selectedItems.find(item => item.size === e.size && item.title === e.title)
+      isAlreadySelected == null ? this.selectedItems.push(e) : isAlreadySelected.shoeCount += e.shoeCount
+      console.log(this.selectedItems)
+      this.$toasted.show("Item added", {
+        theme: "outline",
+        position: "top-right",
+        duration: 2000,
+        iconPack: "fontawesome",
+        icon: "shopping-basket",
+        type: "success"
+      });
+    },
+    deleteItem(index) {
+      this.selectedItems.splice(index, 1)
+    }
   }
 }
 </script>
@@ -22,7 +110,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.navbar {
+  padding-left: 20px;
+  padding-right: 20px;
 }
 </style>
---
+++ src/components/ShoeCard.vue
@@ -0,0 +1,175 @@
+<template>
+  <div id="shoe-card">
+    <b-card class="overflow-hidden" style="width: 600px; height: 380px; background-color: #F5F5F5">
+      <b-row>
+        <b-col cols="7">
+          <b-card-img :src="item.imgSrc" :alt="item.imgAlt"/>
+        </b-col>
+        <b-col cols="5" class="d-flex flex-wrap align-items-center">
+          <h5 style="width: 100%">{{ item.title }}</h5>
+          <b-card-text class="summary">
+            {{ item.summary }}
+          </b-card-text>
+          <b-row class="w-100 justify-content-center">
+            <h5 class="m-0" style="color: #333333">{{ calculatePrice }} $</h5>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row style="padding-bottom: 16px; padding-top: 8px;">
+        <b-col cols="7">
+        <span v-for="i in item.rating" :key="'checked' + i" class="fa fa-star checked"
+              :class="'checked-'+  item.color"/>
+          <span v-for="j in 5 - item.rating" :key="'unchecked' + j" class="fa fa-star"/>
+        </b-col>
+      </b-row>
+      <b-row style="padding-bottom: 8px">
+        <b-col cols="7" class="text-left " style="font-size: 16px; padding-left: 24px">
+          <b class="align-text-top">Sizes</b>
+        </b-col>
+        <b-col cols="5" class="d-flex justify-content-center">
+          <i @click="changeCount('dec')" class="fa fa-minus mx-3 d-flex align-items-center" aria-hidden="true"></i>
+          <i id="count">{{ shoeCount }}</i>
+          <i @click="changeCount('inc')" class="fa fa-plus mx-3 d-flex align-items-center" aria-hidden="true"></i>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="7">
+          <!--          <b-button @click="selectedSize = i" style="height: 32px;" v-for="i in shoeSizes" :key="i"-->
+          <!--                    class="btn-shoe-size">-->
+          <!--            {{ i }}-->
+          <!--          </b-button>-->
+          <b-form-group v-slot="{ ariaDescribedby }">
+            <b-form-radio-group
+                v-model="selectedSize"
+                :options="shoeSizes"
+                buttons
+                :aria-describedby="ariaDescribedby"
+                button-variant="outline-dark"
+            ></b-form-radio-group>
+          </b-form-group>
+
+        </b-col>
+        <b-col cols="5">
+          <b-button @click="selectedTitle = item.title;addToBasket()" :class="item.color" href="#">Add to basket
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoeCard",
+  props: {
+    item: Object,
+  },
+  data() {
+    return {
+      shoeSizes: [5, 6, 7, 8, 9, 10, 11, 12],
+      selectedSize: null,
+      selectedTitle: "",
+      shoeCount: 1,
+      // selectedShoeSize: 5
+    }
+  },
+  computed: {
+    calculatePrice() {
+      if (this.selectedSize > 8) {
+        return this.item.price * 1.2
+      } else {
+        return this.item.price
+      }
+    }
+  },
+  watch: {
+    selectedSize: function (newVal, oldVal) {
+      this.shoeCount = newVal === oldVal ? this.shoeCount : 1
+    }
+  },
+  methods: {
+    changeCount(action) {
+      if (action === "inc" && this.shoeCount < 10) {
+        this.shoeCount += 1;
+      } else if (action === "dec" && this.shoeCount > 1) {
+        this.shoeCount -= 1;
+      }
+    },
+    addToBasket() {
+      if (this.selectedSize != null) {
+        let selectedItem = {
+          title: this.selectedTitle,
+          size: this.selectedSize,
+          shoeCount: this.shoeCount,
+          price: this.calculatePrice
+        }
+        this.$emit("basket", selectedItem);
+      } else {
+        this.$toasted.show("Please select shoe size", {
+          theme: "outline",
+          position: "top-right",
+          duration: 2000,
+          iconPack: "fontawesome",
+          icon: "warning",
+          type: "error"
+        });
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#shoe-card {
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+
+#count {
+  width: 32px;
+  border-color: lightsteelblue;
+  border-style: dashed;
+  font-style: normal;
+}
+
+.pink {
+  background-color: #cca9a8;
+}
+
+.blue {
+  background-color: #4A9BC6;
+}
+
+.checked-pink {
+  color: #cca9a8;
+}
+
+.checked-blue {
+  color: #4A9BC6;
+}
+
+.summary {
+  font-size: small;
+  text-align: start;
+}
+
+.btn-shoe-size {
+  font-size: 8px;
+}
+
+.btn-shoe-size:checked {
+  background: #FFFFFF;
+  box-shadow: none;
+}
+
+.btn-shoe-size:active {
+  background: #FFFFFF;
+  box-shadow: none;
+}
+
+.btn-shoe-size:hover {
+  background: #FFFFFF;
+}
+
+
+</style>
